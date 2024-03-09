c6c99f25d71b5ece49d2a795be85ff67f4ef5219
AShakhmatov <Coluchy@yandex.ru>
2024-03-09 06:34:47 +0600

    add parameters

--- src/test/java/config/ProjectConfig.java
+++ src/test/java/config/ProjectConfig.java
@@ -5,12 +5,9 @@ import org.aeonbits.owner.Config;
 @Config.Sources("classpath:config/${environment}.properties")
 public interface ProjectConfig extends Config {
     @Key("first_name")
-    default String firstName() {
-        return null;
-    }
+    String firstName();
 
     @Key("last_name")
-    default String lastName() {
-        return null;
-    }
+    String lastName();
 }
+
